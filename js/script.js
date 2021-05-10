ee40ebe30abae60b4220f8d193bd8a503b5558f0
fmarabini93 <fmarabini@icloud.com>
2021-05-10 14:22:00 +0200

    second-point-base

--- js/script.js
+++ js/script.js
@@ -7,3 +7,35 @@ var stdPrint = document.getElementById("student");
 for (var key in student) {
     stdPrint.innerHTML += "<li>" + student[key] + "</li>";
 }
+
+var students = [
+    {
+        firstName: "Jim",
+        lastName: "Dean",
+        birthDate: "1/12/1999"
+    },
+    {
+        firstName: "Jane",
+        lastName: "Dave",
+        birthDate: "2/4/2001"
+    },
+    {
+        firstName: "Janet",
+        lastName: "Dick",
+        birthDate: "9/27/1998"
+    },
+    {
+        firstName: "Jason",
+        lastName: "Dawn",
+        birthDate: "10/2/2002"
+    },
+    {
+        firstName: "Jack",
+        lastName: "Dawson",
+        birthDate: "7/29/1998"
+    }
+];
+var stdsPrint = document.getElementById("students");
+for (var key in students) {
+    stdsPrint.innerHTML += "<li>" + students[key].firstName + " " + students[key].lastName + "</li>";
+}
